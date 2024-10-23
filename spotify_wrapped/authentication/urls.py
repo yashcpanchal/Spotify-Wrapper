ea1d3fdce5215e126fd11ecdac698908852d0cfd
Paloma Cardozo <pmc8@gatech.edu>
2024-10-23 19:51:54 -0400

    fixed location of manage module

--- spotify_wrapped/authentication/urls.py
+++ spotify_wrapped/authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('reset_password/', views.request_username_view, name='request_username'),
     path('reset_password/security_question', views.reset_password_view, name='reset_password'),
     path('login_user/<str:username>/', views.login_user, name='login_user'),
+    path('', views.home_view, name='home'),
 ]
--- spotify_wrapped/authentication/views.py
+++ spotify_wrapped/authentication/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def homeView(request):
+def home_view(request):
     return render(request, 'home.html')
