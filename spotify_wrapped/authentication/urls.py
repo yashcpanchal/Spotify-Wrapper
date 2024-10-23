9c365466baa2233ce06bb8501712cb1c53f3add1
yashcpanchal <yashcpanchal41@gmail.com>
2024-10-23 16:37:51 -0400

    adding an authentication app -- using from foodfinder project and reskinning it

---
+++ spotify_wrapped/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('reset_password/', views.request_username_view, name='request_username'),
+    path('reset_password/security_question', views.reset_password_view, name='reset_password'),
+    path('login_user/<str:username>/', views.login_user, name='login_user'),
+]
