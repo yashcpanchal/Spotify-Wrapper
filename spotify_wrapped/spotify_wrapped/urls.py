a1c597ad03d96fa465eccf80f5b8e55655ea7bd2
Paloma Cardozo <pmc8@gatech.edu>
2024-10-23 19:43:29 -0400

    creating home page

--- spotify_wrapped/spotify_wrapped/urls.py
+++ spotify_wrapped/spotify_wrapped/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='homeView'),
 ]
